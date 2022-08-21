3a0ee6bfed0eeee692323a8ea83e87f905919ba0
Mahmoud Sadek <mahmoud.mostafa.sadek@gmail.com>
2022-08-21 18:25:38 +0200

    Initial commit

---
+++ lib/shop_app/login/cubit/shop_login_screen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/cache_helper.dart';
+import 'package:flutter_app/components.dart';
+import 'package:flutter_app/constants.dart';
+import 'package:flutter_app/layout/shop_layout.dart';
+import 'package:flutter_app/shop_app/login/cubit/states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../register/shop_register_screen.dart';
+import 'cubit.dart';
+import 'package:conditional_builder/conditional_builder.dart';
+
+class ShopLoginScreen extends StatelessWidget
+{
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+
+    return BlocProvider(
+      create: (BuildContext context) => ShopLoginCubit(),
+      child: BlocConsumer<ShopLoginCubit, ShopLoginStates>(
+        listener: (context, state) {
+          if(state is ShopLoginSuccessState){
+            if(state.loginModel.status){
+              showToast(text: state.loginModel.message, state: ToastStates.SUCCESS);
+                CacheHelper.saveData(key: 'token', value: state.loginModel.data.token)
+                    .then((value) {
+                      token = state.loginModel.data.token;
+                  navigateAndFinish(context, ShopLayout());
+
+                });
+
+            }else{
+              showToast(text: state.loginModel.message, state: ToastStates.ERROR);
+            }
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'LOGIN',
+                          style: Theme.of(context).textTheme.headline4.copyWith(
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Login now to browse our hot offers',
+                          style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        defaultFormField(
+                          controller: emailController,
+                          type: TextInputType.emailAddress,
+                          validate: (String value) {
+                            if (value.isEmpty) {
+                              return 'please enter your email address';
+                            }
+                          },
+                          label: 'Email Address',
+                          prefix: Icons.email_outlined,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        defaultFormField(
+                          controller: passwordController,
+                          type: TextInputType.visiblePassword,
+                          suffix: ShopLoginCubit.get(context).suffix,
+                          onSubmit: (value)
+                          {
+                            if(formKey.currentState.validate())
+                            {
+                              ShopLoginCubit.get(context).userLogin(
+                                email: emailController.text,
+                                password: passwordController.text,
+                              );
+                            }
+                          },
+                          isPassword: ShopLoginCubit.get(context).isPassword,
+                          suffixPressed: ()
+                          {
+                            ShopLoginCubit.get(context).changePasswordVisibility();
+                          },
+                          validate: (String value) {
+                            if (value.isEmpty) {
+                              return 'password is too short';
+                            }
+                          },
+                          label: 'Password',
+                          prefix: Icons.lock_outline,
+                        ),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        ConditionalBuilder(
+                          condition: state is! ShopLoginLoadingState,
+                          builder: (context) => defaultButton(
+                            function: ()
+                            {
+                              if(formKey.currentState.validate())
+                              {
+                                ShopLoginCubit.get(context).userLogin(
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                );
+                              }
+                            },
+                            text: 'login',
+                            isUpperCase: true,
+                          ),
+                          fallback: (context) =>
+                              Center(child: CircularProgressIndicator()),
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Don\'t have an account?',
+                            ),
+                            defaultTextButton(
+                              function: () {
+                                navigateTo(
+                                  context,
+                                  ShopRegisterScreen(),
+                                );
+                              },
+                              text: 'register',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
